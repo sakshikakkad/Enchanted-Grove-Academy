e4835f7514789a198cdfa0157b810eeebbe6650e
unknown <alina27p@gmail.com>
2024-03-06 17:01:48 -0500

    implemented chasing

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -7,7 +7,8 @@ public class AIController : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
     private Animator animator;
-    public GameObject movingWayPoint;
+    public GameObject player;
+    private NavMeshHit hit;
 
     public enum AIState
     {
@@ -21,12 +22,56 @@ public class AIController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        aiState = AIState.Chase;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();   
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if ()
+        // {
+            
+        // } 
         
+        switch (aiState)
+        {
+            case AIState.Idle:
+                if (animator != null)
+                {
+                    animator.Play("Base Layer.Idle");
+                };
+                break;
+            case AIState.Chase:
+                if (animator != null)
+                {
+                    animator.Play("Base Layer.Walk");
+                };
+                Vector3 futureTarget = calculateDestination();
+                if (!NavMesh.Raycast(navMeshAgent.transform.position, futureTarget, out hit, NavMesh.AllAreas))
+                {
+                    navMeshAgent.SetDestination(futureTarget);
+                }
+                break;
+            // case AIState.ChaseMovingWayPoint:
+            //     animator.SetFloat("vely", navMeshAgent.velocity.magnitude / navMeshAgent.speed);
+            //     Vector3 futureTarget = calculateDestination();
+            //     if (!NavMesh.Raycast(navMeshAgent.transform.position, futureTarget, out hit, NavMesh.AllAreas))
+            //     {
+            //         navMeshAgent.SetDestination(futureTarget);
+            //     }
+            //     break;
+            default:
+                break;
+        }
+    }
+
+    public Vector3 calculateDestination()
+    {
+        float dist = (navMeshAgent.transform.position - player.transform.position).magnitude;
+        float lookAheadT = Mathf.Clamp(dist / (navMeshAgent.speed), 0.1f, 10f);
+        Vector3 futureTarget = player.transform.position + lookAheadT * player.GetComponent<VelocityReporter>().velocity;
+        return futureTarget;
     }
 }
