eaf67038327ef850a69f1363b54071e58dfdad62
Sakshi Kakkad <sakshikakkad@gmail.com>
2024-04-15 01:12:10 -0400

    fixed weird lighting bug when navigating to main scene from start

--- Assets/Scripts/Scene Navigation/GameStarter.cs
+++ Assets/Scripts/Scene Navigation/GameStarter.cs
@@ -6,17 +6,6 @@ using UnityEngine;
 
 public class GameStarter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartGame()
     {
